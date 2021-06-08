a3992ac2041f8ebac8ee4ba48c970e4aded65928
josdem <joseluis.delacruz@gmail.com>
2021-06-08 16:37:12 -0400

    Adding data guid value

---
+++ src/main/java/com/jos/dem/games/controller/GamesController.java
@@ -0,0 +1,13 @@
+package com.jos.dem.games.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GamesController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index";
+  }
+}

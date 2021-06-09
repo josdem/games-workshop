c431d8ba850bfe70c2633c070f0b80f442301e5f
josdem <joseluis.delacruz@gmail.com>
2021-06-09 11:01:06 -0400

    #2 Completing Bear, Human, Gun logic

--- src/main/resources/static/js/bear-human-gun.js
+++ src/main/resources/static/js/bear-human-gun.js
@@ -1,3 +1,57 @@
-function getUserChoice(selection) {
-  console.log("Selected: " + selection);
+const MAX_NUMBER = 3;
+var result;
+
+function getResult(userSelection){
+  getWinner(userSelection);
+  console.log("result: " + result);
+  return result;
+}
+
+function generateComputerNumber() {
+  return Math.floor(Math.random() * MAX_NUMBER);
+}
+
+function getComputerChoice() {
+  switch(generateComputerNumber()){
+    case 0: return 'bear';
+    case 1: return 'human';
+    case 2: return 'gun';
+  }
+}
+
+function getWinner(userSelection) {
+  let toEval = userSelection + getComputerChoice();
+  console.log("eval: " + toEval);
+  switch(toEval){
+    case "bearbear":
+      result = 'tie';
+      break;
+    case "humanhuman":
+      result = 'tie';
+      break;
+    case "gungun":
+      result = 'tie';
+      break;
+    case "bearhuman":
+      result = 'user';
+      break;
+    case "beargun":
+      result = 'computer';
+      break;
+    case "humanbear":
+      result = 'computer';
+      break;
+    case "humangun":
+      result = 'user';
+      break;
+    case "gunbear":
+      result = 'computer';
+      break;
+    case "gunhuman":
+      result = 'user';
+      break;
+    default:
+      result = 'nill';
+  }
+
 }

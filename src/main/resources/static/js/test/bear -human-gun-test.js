3f062bc0e69ef4609ac121704b1de480732a7035
josdem <joseluis.delacruz@gmail.com>
2021-06-10 18:00:15 -0400

    #7 Get rid of function keyword in test cases

--- src/main/resources/static/js/test/bear -human-gun-test.js
+++ src/main/resources/static/js/test/bear -human-gun-test.js
@@ -4,34 +4,35 @@ const {score} = require('../bear-human-gun.js');
 const {userScore} = require('../bear-human-gun.js');
 const {computerScore} = require('../bear-human-gun.js');
 
-describe('User selects bear, human, gun and invalid', function() {
-  it('it should select bear', function() {
+describe('User selects bear, human, gun and invalid', () => {
+
+  it('it should select bear', () => {
     let result = game('bear');
     assert.oneOf(result, ['tie','user','computer'], 'Should be a valid value');
   });
 
-  it('it should select human', function() {
+  it('it should select human', () => {
     let result = game('human');
     assert.oneOf(result, ['tie','user','computer'], 'Should be a valid value');
   });
 
-  it('it should select gun', function() {
+  it('it should select gun', () => {
     let result = game('gun');
     assert.oneOf(result, ['tie','user','computer'], 'Should be a valid value');
   });
 
-  it('it should select undefined', function() {
+  it('it should select undefined', () => {
     let result = game('undefined');
     assert.equal(result, 'nill', 'Should be undefined');
   });
 });
 
-describe('Game updates score', function() {
-  it('it should verify user score is updated', function() {
+describe('Game updates score', () => {
+  it('it should verify user score is updated', () => {
     assert.isTrue(userScore() >= 0, 'should increment user score');
   });
 
-  it('it should verify computer score is updated', function() {
+  it('it should verify computer score is updated', () => {
     assert.isTrue(computerScore() >= 0, 'should increment computer score');
   });
 });

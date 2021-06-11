9c630d048a50f31ded30812366897c81f3e46bf2
josdem <joseluis.delacruz@gmail.com>
2021-06-11 15:10:29 -0400

    #9 Reading image user keyuword

--- src/main/resources/static/js/giphy-search.js
+++ src/main/resources/static/js/giphy-search.js
@@ -1,5 +1,5 @@
 const getGiphyImage = () => {
-  let query = config.url + "?api_key=" + config.key + "&q=pizza&limit=1&offset=0&rating=g&lang=en";
+  let query = config.url + "?api_key=" + config.key + "&q=" + $('#keyword').val() + "&limit=1&offset=0&rating=g&lang=en";
 
   fetch(query)
     .then(response => response.json())

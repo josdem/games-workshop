66925e049caa07ac854c8af7410768322dd88ddf
josdem <joseluis.delacruz@gmail.com>
2021-06-12 12:56:18 -0400

    #12 Add function to validate inputs

--- src/main/resources/static/js/canvas-modifier.js
+++ src/main/resources/static/js/canvas-modifier.js
@@ -1,9 +1,24 @@
 const updateCanvasColor = () => {
+    const red = $('#red').val();
+    const green = $('#green').val();
+    const blue = $('#blue').val();
+
+    validateInputs(red, green, blue);
+
     $('#canvas').drawRect({
-        fillStyle: "rgb(" + $('#red').val() + "," + $('#green').val() + "," + $('#blue').val() + ")",
+        fillStyle: "rgb(" + red + "," + green + "," + blue + ")",
         x: 0, y: 0,
         fromCenter: false,
         width: config.width,
         height: config.height
     });
 }
+
+const validateInputs = (red, green, blue) => {
+  if(red < 0 || red > 255)
+    alert("Seems like it does not have a valid value: " + red);
+  if(green < 0 || green > 255)
+    alert("Seems like it does not have a valid value: " + green);
+  if(blue < 0 || blue > 255)
+    alert("Seems it does not have a valid value: " + blue);
+};

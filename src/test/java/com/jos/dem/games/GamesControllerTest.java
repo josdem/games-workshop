12be5550310d745aa9f8de88bd66802e8399e9fc
josdem <joseluis.delacruz@gmail.com>
2021-06-12 17:35:47 -0400

    #13 Adding autowired with Lombok

--- src/test/java/com/jos/dem/games/GamesControllerTest.java
+++ src/test/java/com/jos/dem/games/GamesControllerTest.java
@@ -1,5 +1,6 @@
 package com.jos.dem.games;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class GamesControllerTest {
 
-  @Autowired private WebTestClient webTestClient;
+  private final WebTestClient webTestClient;
 
   @Test
   @DisplayName("direct to index")

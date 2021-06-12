70b7abf720336630b9b6bbd6d41f8196818e4568
josdem <joseluis.delacruz@gmail.com>
2021-06-12 17:19:00 -0400

    #13 Fixing controller return

--- src/main/java/com/jos/dem/games/controller/GamesController.java
+++ src/main/java/com/jos/dem/games/controller/GamesController.java
@@ -2,27 +2,28 @@ package com.jos.dem.games.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class GamesController {
 
   @RequestMapping("/")
-  public String index() {
-    return "index";
+  public Mono<String> index() {
+    return Mono.just("index");
   }
 
   @RequestMapping("/behugu")
-  public String behugu() {
-    return "behugu";
+  public Mono<String> behugu() {
+    return Mono.just("behugu");
   }
 
   @RequestMapping("/giphy")
-  public String giphy() {
-    return "giphy";
+  public Mono<String> giphy() {
+    return Mono.just("giphy");
   }
 
   @RequestMapping("/crecol")
-  public String crecol() {
-    return "crecol";
+  public Mono<String> crecol() {
+    return Mono.just("crecol");
   }
 }

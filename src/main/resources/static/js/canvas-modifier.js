ed1736c01d39f6a853ef8e3638ac70f34531387c
josdem <joseluis.delacruz@gmail.com>
2021-06-12 12:32:53 -0400

    #12 Reading user input

--- src/main/resources/static/js/canvas-modifier.js
+++ src/main/resources/static/js/canvas-modifier.js
@@ -1,6 +1,6 @@
 const updateCanvasColor = () => {
     $('#canvas').drawRect({
-        fillStyle: 'steelblue',
+        fillStyle: "rgb(" + $('#red').val() + "," + $('#green').val() + "," + $('#blue').val() + ")",
         x: 0, y: 0,
         fromCenter: false,
         width: config.width,

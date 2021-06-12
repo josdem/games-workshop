e591d4734a6d97010743e665ff7f18075bdb5b9e
josdem <joseluis.delacruz@gmail.com>
2021-06-12 11:30:49 -0400

    #12 Adding jcanvas library and JS script

---
+++ src/main/resources/static/js/canvas-modifier.js
@@ -0,0 +1,9 @@
+const updateCanvasColor = () => {
+    $('#canvas').drawRect({
+        fillStyle: 'steelblue',
+        x: 0, y: 0,
+        fromCenter: false,
+        width: 300,
+        height: 150
+    });
+}

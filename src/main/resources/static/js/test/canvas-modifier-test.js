80f84bf05f616f9347f048fa06f3ec7068e008e0
josdem <joseluis.delacruz@gmail.com>
2021-06-12 13:22:18 -0400

    #12 Validating red green and blue basic values

--- src/main/resources/static/js/test/canvas-modifier-test.js
+++ src/main/resources/static/js/test/canvas-modifier-test.js
@@ -3,9 +3,19 @@ const {canvas} = require('../canvas-modifier.js');
 
 describe('User input an invalid red value', () => {
 
-  it('it should validate red value', () => {
+  it('it should validate red values', () => {
     assert.isFalse(canvas(-1, 1, 1), 'Should validate negative');
     assert.isFalse(canvas(256, 1, 1), 'Should validate huge value');
+  });
+
+  it('it should validate green values', () => {
+    assert.isFalse(canvas(1, -1, 1), 'Should validate negative');
+    assert.isFalse(canvas(1, 256, 1), 'Should validate huge value');
+  });
+
+  it('it should validate blue values', () => {
+    assert.isFalse(canvas(1, 1, -1), 'Should validate negative');
+    assert.isFalse(canvas(1, 1, 256), 'Should validate huge value');
   });
 
 });

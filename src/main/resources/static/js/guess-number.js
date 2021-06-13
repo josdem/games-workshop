a63227c56ec79af0457c69f3fd2eb444b1cfe35f
josdem <joseluis.delacruz@gmail.com>
2021-06-13 14:26:49 -0400

    #16 Adding generate and validate number functionality

--- src/main/resources/static/js/guess-number.js
+++ src/main/resources/static/js/guess-number.js
@@ -1,16 +1,29 @@
 const MAX_NUMBER = 101;
 
-const generateNumber = () => {
-    return Math.floor(Math.random() * MAX_NUMBER)
+class GuessNumberGame {
+    constructor() {
+        this.numberGenerated = Math.floor(Math.random() * MAX_NUMBER)
+        console.log("Number generated: " + this.numberGenerated)
+    }
+}
+
+const guessNumber = () => {
+  let userInput = $('#number').val()
+  if(define(userInput) === 0)
+    $('#message').html('You are correct, the answer is: ' + userInput)
+  if(define(userInput) === -1)
+    $('#message').html('Too low')
+  if(define(userInput) === 1)
+    $('#message').html('Too high')
 }
 
 const define = (userInput) => {
-    let numberGenerated = generateNumber()
-    if(userInput < numberGenerated)
+    if(userInput < game.numberGenerated)
         return -1
-    if(userInput === numberGenerated)
+    if(userInput == game.numberGenerated)
         return 0
     return 1
 }
 
+let game = new GuessNumberGame()
 exports.define = define;

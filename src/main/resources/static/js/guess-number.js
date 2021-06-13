71c1707ba70053e416a43ba1e389b97433591605
josdem <joseluis.delacruz@gmail.com>
2021-06-13 14:49:30 -0400

    #16 Disabling when game over

--- src/main/resources/static/js/guess-number.js
+++ src/main/resources/static/js/guess-number.js
@@ -1,20 +1,27 @@
-const MAX_NUMBER = 101;
+const MAX_NUMBER = 101
+let tries = 0
 
 class GuessNumberGame {
     constructor() {
         this.numberGenerated = Math.floor(Math.random() * MAX_NUMBER)
-        console.log("Number generated: " + this.numberGenerated)
     }
 }
 
 const guessNumber = () => {
   let userInput = $('#number').val()
-  if(define(userInput) === 0)
-    $('#message').html('You are correct, the answer is: ' + userInput)
+  if(define(userInput) === 0){
+      $('#message').html('You are correct, the answer is: ' + userInput)
+      $('#number').prop('disabled', true)
+      return
+  }
+
   if(define(userInput) === -1)
-    $('#message').html('Too low')
+     $('#message').html('Too low')
+
   if(define(userInput) === 1)
     $('#message').html('Too high')
+
+  $('#tries').html('Tries: ' + ++tries)
 }
 
 const define = (userInput) => {
